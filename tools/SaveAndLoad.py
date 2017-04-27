db098a2d19334a1a97be461e599600d01a7aa4ad
nbyby <nbybyyby@126.com>
2017-04-27 22:06:48 +0800

    LOAD new

--- tools/SaveAndLoad.py
+++ tools/SaveAndLoad.py
@@ -9,13 +9,11 @@ def save(outFile,saveObject):
 		out.write("\n")
 
 # 读取保存在一个文件里的对象，一行一个
-def load(inFile,saveObject):
+def load(inFile,numberOfObjects):
 	returnObject = []
 	inList = open(inFile, "r").readlines()
-	i = 0
-	for x in saveObject:
+	for i in range(numberOfObjects):
 		x = json.loads(inList[i])
-		i += 1
 		returnObject.append(x)
 	return returnObject
 
@@ -23,7 +21,7 @@ if __name__ == '__main__':
 	test = [[1,2,3,4,5],[4,5,6,7,{"中文":True}]]
 	test2 = [[1,2,3],[4,5,6]]
 	DIRECTION = ((0,0),(-2,0),(2,0),(0,2),(0,-2))
-	save("out.txt",[test,test2,DIRECTION])
+	save("out.txt",[test,test2])
 	in1 = []
 	in2 = []
 	[in1,in2] = load("out.txt",[in1,in2])
